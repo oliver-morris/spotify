136d9b8bfe6f63e79e8be4ac1fdf096bba06175a
oliver-morris <oliver38morris@gmail.com>
2024-03-31 13:19:39 +0100

    Added getEmbed method to get the track as a html element

--- spotify/Track.py
+++ spotify/Track.py
@@ -66,4 +66,5 @@ class Track(Item):
                     filename=file_name)
             return file_path + "\\" + file_name
 
-
+    def getEmbed(self):
+        return f'<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/{self.id}?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
